69036d472c96ea6a724e99467f23d56701b90c8f
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2022-09-29 18:09:20 -0700

    compiled?

---
+++ build.js
@@ -0,0 +1,20 @@
+const esbuild = require("esbuild");
+const isDev = process.argv[2] === "dev";
+console.log("Building for " + (isDev ? "development" : "production") + "...");
+esbuild.build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  outfile: "scripts/index.js",
+  minify: !isDev,
+  sourcemap: true,
+  platform: "neutral",
+  watch: isDev,
+  external: [
+    "mojang-minecraft",
+    "mojang-minecraft-ui",
+    "mojang-gametest",
+    "mojang-net",
+    "mojang-minecraft-server-admin",
+  ],
+  legalComments: isDev ? "none" : "eof",
+});
--- scripts/lib/Commands/test.js
+++
@@ -1,3 +0,0 @@
-function x(a, b, c) {
-}
-const a;
