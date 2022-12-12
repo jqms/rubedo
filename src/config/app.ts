cceb3d9c559edf3361536c39535902bb75035ed5
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2022-12-12 15:55:11 -0500

    Incremented version

--- src/config/app.ts
+++ src/config/app.ts
@@ -6,7 +6,7 @@
 | This is the version of rubedo recommended to NOT CHANGE could cause bugs.
 |
 */
-export const VERSION = "2.6.2-beta";
+export const VERSION = "2.6.3-beta";
 
 /*
 |--------------------------------------------------------------------------
